ca6f0f61292b2d5974ae93e265b58013d9474162
jerinthomas1404 <jerinthomas1404@gmail.com>
2024-12-22 20:32:03 +0530

    adding lua configuation

---
+++ lua/plugins/init.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.export"] = {},
+          ["core.keybinds"] = { config = { default_keybinds  = true,}},
+          ["core.journal"] = { config = { strategy = "daily", workspace = "notes"}},
+          ["core.export.markdown"]= { config = { extensions = "all" } },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "norg", "go","json", "yaml"
+  		},
+  	},
+  }
+}
